13f87b28d840daeb7faa0e6f90e03c92a43b7486
avs <avs@omp-system.ru>
2022-09-08 18:24:22 +0300

    add code generation for columns

--- omp/code_genereators/gen_directory/defaultOpts.js
+++ omp/code_genereators/gen_directory/defaultOpts.js
@@ -5,8 +5,11 @@ const defaultOpts = {
     "entities": "Stockobjs",
     "entity_hr_name": "Объект учета",
     "entities_hr_name": "Объекты учета",
+    "table_name": "_YOUR_TABLE_NAME_",
+    "sequence_name": "_YOUR_SEQ_NAME_",
     "columns": [{
         "columnName": "CODE",
+        "columnNameRus": "Код",
         "columnDataType": "INTEGER",
         "columnDataLength": 0,
         "dataMemberName": "Code",
@@ -15,12 +18,23 @@ const defaultOpts = {
         "filterMemberType": "long_set"
     }, {
         "columnName": "NUM",
+        "columnNameRus": "Номер",
         "columnDataType": "VARCHAR2",
         "columnDataLength": 256,
         "dataMemberName": "Num",
         "dataMemberType": "COmpStringT",
         "filterMemberName": "Num",
         "filterMemberType": "CLikeSensString"
+    },
+    {
+        "columnName": "CREATE_DATE",
+        "columnNameRus": "Дата создания",
+        "columnDataType": "DATE",
+        "columnDataLength": 0,
+        "dataMemberName": "CreateDate",
+        "dataMemberType": "ITDate",
+        "filterMemberName": "CreateDate",
+        "filterMemberType": "IntervalData"
     }
     ]
 };
